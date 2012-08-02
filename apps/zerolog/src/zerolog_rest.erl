64279176bbe35b1f4a7fbe7433b3c32f3bd0dda6
Bip Thelin <bip@kivra.com>
2012-08-02 15:48:22 +0200

    Add License

--- apps/zerolog/src/zerolog_rest.erl
+++ apps/zerolog/src/zerolog_rest.erl
@@ -1,8 +1,25 @@
-%% @author Bip Thelin <bip.thelin@evolope.se>
-%% @copyright 2010-2011 Evolope.
+%% ----------------------------------------------------------------------------
+%%
+%% zerolog: Log transport that just works with zero effort.
+%%
+%% Copyright 2012 (c) KIVRA.  All Rights Reserved.
+%% http://developer.kivra.com dev@kivra.com
+%%
+%% This file is provided to you under the Apache License, Version 2.0 (the
+%% "License"); you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+%% License for the specific language governing permissions and limitations
+%% under the License.
+%%
+%% ----------------------------------------------------------------------------
 
 -module(zerolog_rest).
--author('Bip Thelin <bip.thelin@evolope.se>').
 
 -behaviour(gen_server).
 
